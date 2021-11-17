47197f78a261c53c083df5e3dbfe2abdb95db822
hsLee <gntmddl4261@gmail.com>
2021-11-17 16:16:15 +0900

    1. DrawerLayout 추가

--- app/src/main/java/com/hu/myroutine/MainActivity.kt
+++ app/src/main/java/com/hu/myroutine/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hu.myroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.hu.myroutine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +15,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val topAppBar = binding.appBarMain.topAppBar
+
+//        setSupportActionBar(topAppBar)
+
+        ActionBarDrawerToggle(this, drawerLayout, topAppBar, 0, 0)
 
     }
-
 }
