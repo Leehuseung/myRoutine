88254ab9d527c223b62d7afd90876cff5629ee51
hsLee <gntmddl4261@gmail.com>
2021-11-17 22:49:21 +0900

    1. 운동추가Activity 구현 2. 운동추가Activity appBar 구현

---
+++ app/src/main/java/com/hu/myroutine/ExerciseAddActivity.kt
@@ -0,0 +1,35 @@
+package com.hu.myroutine
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.hu.myroutine.databinding.ActivityExerciseAddBinding
+
+class ExerciseAddActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityExerciseAddBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val topAppBar = binding.addExerciseBarMain.topAppBar
+
+        topAppBar.setNavigationOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.confirm -> {
+                    Toast.makeText(applicationContext,"운동 등록하기",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
--- app/src/main/java/com/hu/myroutine/MainActivity.kt
+++ app/src/main/java/com/hu/myroutine/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.hu.myroutine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.hu.myroutine.databinding.ActivityMainBinding
+import com.hu.myroutine.databinding.NavHeaderMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +20,14 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val topAppBar = binding.appBarMain.topAppBar
 
-//        setSupportActionBar(topAppBar)
+        val navHeaderView = binding.navView.getHeaderView(0)
+        val navHeaderBinding = NavHeaderMainBinding.bind(navHeaderView)
+
+        navHeaderBinding.addExercise.setOnClickListener {
+            val intent = Intent(this,ExerciseAddActivity::class.java)
+            startActivity(intent)
+        }
 
         ActionBarDrawerToggle(this, drawerLayout, topAppBar, 0, 0)
-
     }
 }
